8b1dadeb5117909af54bfb58f4cb9ea8054cc07e
leandrl-pixel <leandrl@uci.edu>
2026-04-21 12:08:38 -0700

    Update prelearningtestW4

--- Assets/Prelearning/W4Graphs/PrelearnTestW4Event.cs
+++ Assets/Prelearning/W4Graphs/PrelearnTestW4Event.cs
@@ -8,17 +8,31 @@ public class PrelearnTestW4Event : MonoBehaviour
     public static class EventNames
     {
         public static string YourEventNameHere = "EventName";
-
-
     }
+
     [UnitTitle("On Your Event Name")]
     [UnitCategory("Events\\MyEvents")]
-
-    public class RelevantClassNameHere: EventUnit<DialogueNode> { }
-    public override EventHook GetHook(GraphReference reference)
+    public class RelevantClassNameHere : EventUnit<DialogueNode>
     {
-        return new EventHook(EventNames.YourEventNameHere);
+        [DoNotSerialize]
+        public ValueOutput Result { get; private set; }
+
+        protected override bool register => true;
+
+        protected override void Definition()
+        {
+            base.Definition();
+            Result = ValueOutput<DialogueNode>(nameof(Result));
+        }
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            return new EventHook(EventNames.YourEventNameHere);
+        }
+
+        protected override void AssignArguments(Flow flow, DialogueNode data)
+        {
+            flow.SetValue(Result, data);
+        }
     }
-
-
 }
